b4ed66df4f09cf7ac860a85d5b202e4da6758a68
Rara Syifa Qaisa <rarasyifaqaisa@email.com>
2026-05-02 20:46:29 +0700

    fix: remove hardcoded secrets, use env vars

--- backend/app/supabase_client.py
+++ backend/app/supabase_client.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("https://ppvppuqaaxwjwkdbthwm.supabase.co")
-SUPABASE_KEY = os.getenv("sb_secret_CY3R-5oFZMFestjhRC0gnw_dHc3wnil")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
